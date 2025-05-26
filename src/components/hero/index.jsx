1f8034ef7f5abcd7f886416611110617adfb1078
Usman Hameed <usmanhameed1790@gmail.com>
2025-05-26 17:43:19 +0500

    Background color setup

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -4,8 +4,8 @@ function Hero()
 {
     return (
         <div className="home-hero">
-        <h1>Welcome to BlogSphere</h1>
-        <p>Discover amazing stories, write your own, and connect with the community. Express yourself and be inspired.</p>
+        <h1 className="display-3 fw-bold"> Welcome to BlogSphere </h1>
+        <p className='lead'>Discover amazing stories, write your own, and connect with the community. Express yourself and be inspired.</p>
         <button>Explore Blogs</button>
         </div>
     );
